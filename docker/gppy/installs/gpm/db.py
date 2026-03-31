be493ac1dcf548ecab185e0d82ef49857f5235cc
GridPresence <grid.presence@gmx.com>
2026-03-31 19:34:20 +0100

    Dev step: 2026-03-31_19-34-20

--- docker/gppy/installs/gpm/db.py
+++ docker/gppy/installs/gpm/db.py
@@ -24,9 +24,10 @@ class Database:
             host=os.getenv("MyServer"),
             database=os.getenv("MyDb"),
         )
-        self._engine = create_engine(
-            self._url, echo=True, insertmanyvalues_page_size=100
-        )
+        # self._engine = create_engine(
+        #     self._url, echo=True, insertmanyvalues_page_size=100
+        # )
+        self._engine = create_engine(self._url, echo=True)
         Base.metadata.drop_all(bind=self._engine, tables=[Track.__table__])
         Base.metadata.create_all(self._engine)
         self._session = Session(self._engine)
