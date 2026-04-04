e8588abab1520fa7269718aa624fa56d3c8b497d
GridPresence <grid.presence@gmx.com>
2026-04-04 15:06:12 +0100

    Dev step: 2026-04-04_15-06-12

--- docker/gppy/installs/gpm/db.py
+++ docker/gppy/installs/gpm/db.py
@@ -75,7 +75,11 @@ class Track(Base):
     def __init__(self, trk: FlacFile):
         # print("TRK")
         # print(trk)
-        self.artist = trk["artist"]
+        try:
+            self.artist = trk["albumartist"]
+        except KeyError:
+            self.artist = trk["artist"]
+
         self.album = trk["album"]
         self.bps = trk["bits_per_sample"]
         self.channels = trk["channels"]
