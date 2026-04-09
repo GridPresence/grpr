36ec68d1c1bb24f98cacf06e17d6516035a1a544
GridPresence <grid.presence@gmx.com>
2026-04-09 11:23:49 +0100

    Dev step: 2026-04-09_11-23-49

--- docker/gppy/installs/gpm/generate.py
+++ docker/gppy/installs/gpm/generate.py
@@ -10,12 +10,19 @@ class Generator:
     def __init__(self, target: Path, source: Database):
         """Run here"""
         self._artists: str = []
+        self._albums = []
         self._source: Database = source
         self._session = self._source.session
 
         self._get_artists()
+        self._get_artists_albums()
 
     def _get_artists(self):
         for value in self._session.query(Track.artist).distinct():
             self._artists.append(value[0])
         print(self._artists)
+
+    def _get_artists_albums(self):
+        for value in self._session.query(Track.artist, Track.album).distinct():
+            self._albums.append(value)
+        print(self._albums)
