2a2e8efa0cf8ae81313f8f2364171e82988e67b8
Jeremy Pavier <grid.presence@gmx.com>
2026-04-13 15:54:48 +0100

    Dev step: 2026-04-13_15-54-48

--- docker/gppy/installs/gpm/generate.py
+++ docker/gppy/installs/gpm/generate.py
@@ -13,6 +13,7 @@ class Generator:
         self._albums = []
         self._source: Database = source
         self._session = self._source.session
+        self._target: Path = target
 
         self._get_artists()
 
@@ -25,21 +26,27 @@ class Generator:
             self._get_artist_albums(art)
 
     def _get_artist_albums(self, artist: str):
-        self._albums = []
+        albums = []
         for value in self._session.query(Track.album, Track.date).distinct().where(Track.artist==artist).order_by(Track.date):
             self._albums.append(value[0])
         print(f"* {artist}")
-        for item in self._albums:
-            # print(f"\t {item}")
-            self._get_trax(artist, item)
+        if len(albums) > 2:
+            self._trax = []
+            fpath: Path = self._target.joinpath(artist,f"{artist}.m3u")
+            for item in albums:
+                # print(f"\t {item}")
+                self._get_trax(artist, item)
+            with open(fpath,"w",encoding="utf8") as fyle:
+                for nitem in self._trax:
+                    fyle.write(nitem)
+                    fyle.write("\n|")
+            fyle.close()
     
     def _get_trax(self, artist: str, album: str):
-        self._trax = []
         for value in self._session.query(Track.file, Track.title, Track.length, Track.path).where(Track.artist==artist, Track.album==album).order_by(Track.file):
             tpath: Path = Path(value[3])
             subpath: Path = Path(tpath.parts[1], tpath.parts[2])
-            m3ustr = f"#EXTINF: {value[2]},{artist} - {value[1]}"
+            m3ustr = f"#EXTINF:{value[2]},{artist} - {value[1]}"
             self._trax.append(m3ustr)
             self._trax.append(subpath)
-        for nitem in self._trax:
-            print(f"\t{nitem}")
+        
